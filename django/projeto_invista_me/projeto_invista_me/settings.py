281251962360cc336cce3ce62bb31da13a389159
Carlos Rodrigues <carlosrodriguesjf@gmail.com>
2023-04-06 17:11:16 -0300

    Site usando django - até aula 19

--- django/projeto_invista_me/projeto_invista_me/settings.py
+++ django/projeto_invista_me/projeto_invista_me/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "invista_me"
+    "invista_me",
+    "crispy_forms",
+    "crispy_bootstrap5"
+
+
 ]
 
 MIDDLEWARE = [
@@ -111,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
